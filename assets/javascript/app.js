dd4a202b5886a43b340b34fe93db4ae1a3393b78
benitezmarcos <marcosbenitez@Marcoss-MacBook-Pro.local>
2019-08-24 18:29:25 -0700

    small changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -156,11 +156,11 @@ function scoreRender(){
     const scorePerCent = Math.round(100 * score/questions.length);
     
     // choose the image based on the scorePerCent
-    let img = (scorePerCent >= 80) ? "img/5.png" :
-              (scorePerCent >= 60) ? "img/4.png" :
-              (scorePerCent >= 40) ? "img/3.png" :
-              (scorePerCent >= 20) ? "img/2.png" :
-              "img/1.png";
+    let img = (scorePerCent >= 80) ? "/assets/images/5.png" :
+              (scorePerCent >= 60) ? "/assets/images/4.png" :
+              (scorePerCent >= 40) ? "/assets/images/3.png" :
+              (scorePerCent >= 20) ? "/assets/images/2.png" :
+            //   (scorePerCent >= 10) ? "assets/images/1.png":
     
     scoreDiv.innerHTML = "<img src="+ img +">";
     scoreDiv.innerHTML += "<p>"+ scorePerCent +"%</p>";
